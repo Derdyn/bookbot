c17cef34fc6f778453672b8bab26aa0c7a1e8346
Derdyn <puma2113@gmail.com>
2025-07-13 18:31:41 -0400

    Added get book functionality

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        get_contents = f.read()
+    return get_contents
+
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    print(book)
+
+
+main()
