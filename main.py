e1a4fdb496fdc52ea3fd57fe994dd80bcf429819
Derdyn <puma2113@gmail.com>
2025-08-19 20:20:19 -0400

    Created stats.py and moved word count to it

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+from stats import word_count
+
 def get_book_text(path):
     with open(path) as f:
         get_contents = f.read()
     return get_contents
 
-def word_count(book):
-    text = get_book_text(book)
-    count = len(text.split())
-    return count
-
 def main():
-    count = word_count("./books/frankenstein.txt")
+    book = "./books/frankenstein.txt"
+    text = get_book_text(book)
+    count = word_count(text)
     print(f"{count} words found in the document")
 
 
