9c4ccd8042ebc3b1fca30ad4a72fbeb9aca17828
Derdyn <puma2113@gmail.com>
2025-08-19 20:12:28 -0400

    Added word count to main

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from stats import word_count
-from stats import char_count
-
 def get_book_text(path):
     with open(path) as f:
         get_contents = f.read()
     return get_contents
 
+def word_count(book):
+    text = get_book_text(book)
+    count = len(text.split())
+    return count
+
 def main():
-    book = get_book_text("./books/frankenstein.txt")
-    print(book)
+    count = word_count("./books/frankenstein.txt")
+    print(f"{count} words found in the document")
 
 
 main()
-word_count()
-char_count = char_count()
-print(char_count)
