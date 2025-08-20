e070828dd361f9e8afa6146abeee9264cae73227
Derdyn <puma2113@gmail.com>
2025-08-19 20:46:59 -0400

    Added char count to stats.py and imported to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import char_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,7 +10,9 @@ def main():
     book = "./books/frankenstein.txt"
     text = get_book_text(book)
     count = word_count(text)
+    chars = char_count(text)
     print(f"{count} words found in the document")
+    print(chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def word_count(text):
     count = len(text.split())
     return count
+
+# Returns how many times each character appears in text
+def char_count(text):
+    chars = {}
+    text_lower = text.lower()
+    for i in text_lower:
+      chars[i] = chars.get(i, 0) + 1
+    return chars
